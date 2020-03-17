c7e61e7bdbb12df59508c6aee46deed589e86318
Albert Cervantes <cervanalbert@gmail.com>
2020-03-16 17:11:43 -0700

    added logic for quiz screen

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+// Question used in quiz
+var questions = [{
+    question: "Q1: This is a test: Choose 1",
+    choices: ["1","2","3","4"],
+    answer: 0
+},{
+    question: "Q2: This is test 2: choose 3",
+    choices: ["1","2","3","4"],
+    answer: 2
+}]
+
 
 // Selectors
 var welcomeEl = document.querySelector(".welcome");
 var timeEl = document.querySelector(".time");
 var scoreEl = document.querySelector(".score");
 var startBtn = document.querySelector(".startBtn");
+var questionEl = document.querySelector(".question");
+var questionTextEl = document.querySelector(".questionText");
+
+var choicesEl = document.querySelector(".choices");
+var lineBreak = document.createElement("hr");
+
+var choiceBtn0 = document.createElement("button");
+var choiceBtn1 = document.createElement("button");
+var choiceBtn2 = document.createElement("button");
+var choiceBtn3 = document.createElement("button");
 
 // Variable for number of seconds
 var secondsLeft = 5;
@@ -32,13 +53,29 @@ function startScreen() {
 }
 
 function quizScreen() {
+    choiceBtn0.setAttribute("class", "button choiceBtn0");
+    choiceBtn1.setAttribute("class", "button choiceBtn1");
+    choiceBtn2.setAttribute("class", "button choiceBtn2");
+    choiceBtn3.setAttribute("class", "button choiceBtn3");
+    
+    questionTextEl.textContent = questions[0].question;
+    questionEl.append(lineBreak);
 
+    choiceBtn0.textContent = questions[0].choices[0];
+    choiceBtn1.textContent = questions[0].choices[1];
+    choiceBtn2.textContent = questions[0].choices[2];
+    choiceBtn3.textContent = questions[0].choices[3];
+
+    choicesEl.append(choiceBtn0);
+    choicesEl.append(choiceBtn1);
+    choicesEl.append(choiceBtn2);
+    choicesEl.append(choiceBtn3);
 }
 
 startBtn.addEventListener("click", function(){
     startBtn.remove();
-    welcomeEl.textContent = "Doing Quiz";
     setTimer();
+    quizScreen();
 });
 
 
